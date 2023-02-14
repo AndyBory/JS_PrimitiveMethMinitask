735d05a1786fded41806fe36f65f6565bd9bb6ca
AndyBory <andyborysov@gmail.com>
2023-02-14 12:59:29 +0200

    first

---
+++ assets/PrimitiveMethMinitask.js
@@ -0,0 +1,30 @@
+// 1. Напишите функцию, которая принимает строку и возвращает количество слов в ней.
+
+function numberOfLines (str) {
+  let words = str.trim().split(' ');
+  console.log(words);
+  return words.length;
+}
+
+const firstStr = '           test string aest test new yes 654654 hello     ';
+console.log(numberOfLines(firstStr));
+
+
+// 2. Напишите функцию, которая получает строку и возвращает размер самого большого слова в ней.
+
+function sizeLargestWord (str) {
+  const words = str.split(' ');
+  console.log(words);
+  let result = '';
+  let size = 0;
+  for(let i = 0; i < words.length; i++) {
+    const word = words[i];
+    if (word.length > size) {
+      size = word.length
+    }
+    result = size;
+  }
+  return result;
+}
+const secondStr = 'test string aest test new yes largestword';
+console.log(sizeLargestWord(secondStr));
